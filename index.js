f5e20997b39b66ea0683208dedbb4760331dbaf5
koinoniays2 <koinonia_ys@naver.com>
2024-02-21 14:14:32 +0900

    [update] 패치 수정

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const serviceKey = 'b26f3923-0250-4ed3-8329-54b04f6af8a2';
 
 const fetchData = async () => {
     try {
-        await fetch(`http://api.kcisa.kr/openapi/API_TOU_052/request?serviceKey=${serviceKey}&numOfRows=${numOfRows}&pageNo=${pageNo}`,{
+        const response = fetch(`http://api.kcisa.kr/openapi/API_TOU_052/request?serviceKey=${serviceKey}&numOfRows=${numOfRows}&pageNo=${pageNo}`,{
             headers: {
             accept: "application/json"
             }
@@ -17,7 +17,7 @@ const fetchData = async () => {
         console.error('There was a problem with the fetch operation:', error);
     }
 };
-
+/*
 const displayData = (data) => {
     const dataContainer = document.getElementById('data-container');
     dataContainer.innerHTML = ''; // Clear previous data
@@ -27,7 +27,5 @@ const nextPage = () => {
     pageNo++;
     fetchData();
 };
-// document.getElementById('next-button').addEventListener('click', nextPage);
-
-// 페이지 로드시 초기 데이터를 가져옵니다.
+document.getElementById('next-button').addEventListener('click', nextPage); */
 fetchData();
