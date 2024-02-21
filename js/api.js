0e361e859fb9d21bde40f63d51bc38d93523e6d7
koinoniays2 <koinonia_ys@naver.com>
2024-02-21 16:41:25 +0900

    api 수정

--- js/api.js
+++ js/api.js
@@ -1,31 +1,21 @@
-let pageNo = 1;
-const numOfRows = 100;
 const serviceKey = 'b26f3923-0250-4ed3-8329-54b04f6af8a2';
 
 const fetchData = async () => {
     try {
-        const response = fetch(`http://api.kcisa.kr/openapi/API_TOU_052/request?serviceKey=${serviceKey}&numOfRows=${numOfRows}&pageNo=${pageNo}`,{
+        const response = await fetch(`http://api.kcisa.kr/openapi/API_TOU_052/request?serviceKey=${serviceKey}`, {
             headers: {
-            accept: "application/json"
+                accept: "application/json"
             }
-        })
-        .then((response) => response.json())
-        .then((json) => {
-          console.log(json);
         });
+        // console.log(response);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const json = await response.json();
+        let vegan = json?.response?.body?.items?.item?.filter((item) => item?.information?.includes("채식 메뉴 있음"));
+        console.log(vegan);
     } catch (error) {
         console.error('There was a problem with the fetch operation:', error);
     }
 };
-/*
-const displayData = (data) => {
-    const dataContainer = document.getElementById('data-container');
-    dataContainer.innerHTML = ''; // Clear previous data
-    dataContainer.innerHTML = `<p>페이지: ${pageNo}</p>`;
-};
-const nextPage = () => {
-    pageNo++;
-    fetchData();
-};
-document.getElementById('next-button').addEventListener('click', nextPage); */
 fetchData();
